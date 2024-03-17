9111f4cf1d74bfcea6adf5b9b4c18663d2c32b0d
Abhishek Satpathy <108116233+AbhishekSatpathy4848@users.noreply.github.com>
2024-03-17 08:05:50 +0000

    add image upload feature

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+import base64, binascii
 from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
+
+
+def decode_base_64(image_data):
+    try:
+        image = base64.b64decode(image_data.split(",")[1], validate=True)
+        return image
+    except binascii.Error as e:
+        print(e)
+
+def save_image(image, image_name):
+    with open(image_name, "wb") as f:
+        f.write(image)
 
 @app.route("/")
 def index():
@@ -8,8 +21,10 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload(): 
-    print(request.form["content"])
-    return Response("OK");
+    image = decode_base_64(request.json["image_data"]);
+    save_image(image, "uploaded_image.png");
+    return Response(status=200);
 
 if __name__  == "__main__":
     app.run(debug=True)
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,9 @@
 let imageDataUrl;
 
+document.addEventListener('DOMContentLoaded', function() {
+    // document.getElementById('loading_message').style.display = "none";
+})
+
 document.getElementById("fileInput").onchange = function() {
     var previewImg = document.getElementById('previewImg');
     var file = document.querySelector('input[type=file]').files[0];
@@ -7,6 +11,7 @@ document.getElementById("fileInput").onchange = function() {
 
     reader.onloadend = function() {
         previewImg.src = reader.result;
+        imageDataUrl = reader.result;
     }
 
     if (file) {
@@ -18,19 +23,22 @@ document.getElementById("fileInput").onchange = function() {
 
 document.getElementById("uploadForm").onsubmit = async function(e) {
     e.preventDefault();
-    console.log(imageDataUrl);
-    // let fileInput = document.getElementById("fileInput");
-    // file = fileInput.files[0];
-    // console.log(file.readAsDataURL);
-    // let options = {
-    //     method: 'POST',
-    //     body: file,
-    //     headers: {
-    //         "Content-type": "application/json; charset=UTF-8"
-    //     }
-    // }
 
-    // await fetch("/upload", options);
+    document.getElementById("loading_message").style.display = "block";
+    let response = await post_image_to_endpoint(imageDataUrl, "/upload");
+    document.getElementById("loading_message").style.display = "none";
 
-    // alert("Image uploaded successfully!");
+    console.log(response);
 };
+
+async function post_image_to_endpoint(image_data, endpoint){
+    let options = {
+        method: 'POST',
+        body: JSON.stringify({"image_data": imageDataUrl}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }
+    let response = await fetch("/upload", options);
+    return response;
+}
